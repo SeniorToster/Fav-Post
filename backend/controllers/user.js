e5a3b0423d5d816e4c654fa891e7bd0295526006
SeniorToster <seraleks04@gmail.com>
2023-04-28 03:27:06 +0300

    создан вход для пользователя

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -9,9 +9,11 @@ const { Users } = require('../models');
 async function registrationUser(req, res, next) {
   try {
     const errors = validationResult(req);
+
     if (!errors.isEmpty()) {
       throw apiError.badRequest('Ошибка валидации', errors.array());
     }
+
     const { email, name, password } = req.body;
     const isUser = await Users.findOne({ where: { email: email } });
 
@@ -24,21 +26,32 @@ async function registrationUser(req, res, next) {
     const hashPassword = await bcrypt.hash(password, 7);
     const id = uuid.v4();
 
-    await Users.create({
+    const user = await Users.create({
       id,
       name,
       email,
       password: hashPassword,
     });
 
-    const tokens = generationTokens({ id, name, email });
-    await saveToken(id, tokens.refreshToken);
+    const tokens = generationTokens({
+      id: user.id,
+      name: user.name,
+      email: user.email,
+    });
+    await saveToken(user.id, tokens.refreshToken);
 
     res.cookie('refreshToken', tokens.refreshToken, {
       maxAge: 30 * 24 * 60 * 60 * 1000,
       httpOnly: true,
     });
-    return res.json({ ...tokens, user: { id, name, email } });
+    return res.json({
+      ...tokens,
+      user: {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+      },
+    });
   } catch (error) {
     next(error);
   }
@@ -47,13 +60,15 @@ async function registrationUser(req, res, next) {
 async function loginUser(req, res, next) {
   try {
     const errors = validationResult(req);
+
     if (!errors.isEmpty()) {
       throw apiError.badRequest('Ошибка валидации', errors.array());
     }
+
     const { email, password } = req.body;
     const user = await Users.findOne({ where: { email: email } });
 
-    if (user) {
+    if (!user) {
       throw apiError.badRequest('Неверный почтовый адрес');
     }
 
@@ -63,14 +78,26 @@ async function loginUser(req, res, next) {
       throw apiError.badRequest('Неверный пароль');
     }
 
-    const tokens = generationTokens({ id, name, email });
-    await saveToken(id, tokens.refreshToken);
+    const tokens = generationTokens({
+      id: user.id,
+      name: user.name,
+      email: user.email,
+    });
+    await saveToken(user.id, tokens.refreshToken);
 
     res.cookie('refreshToken', tokens.refreshToken, {
       maxAge: 30 * 24 * 60 * 60 * 1000,
       httpOnly: true,
     });
-    return res.json({ ...tokens, user: { id, name, email } });
+
+    return res.json({
+      ...tokens,
+      user: {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+      },
+    });
   } catch (error) {
     next(error);
   }
